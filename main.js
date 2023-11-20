0a55b187ba210ece6c94977ceac10de94144cc4d
Oksana <oksana.korobko.fd@gmail.com>
2023-11-20 18:06:49 +0200

    implemented fetch-request

--- main.js
+++ main.js
@@ -8,28 +8,49 @@
 // temperature:400
 
 let page = 'Start';
-let planets = document.querySelectorAll('.planet');
-planets.forEach(elem => {
+let planetsDiv = document.querySelectorAll('.planet');
+let divLoader = document.querySelector('#loader');
+
+function showLoader(){
+    divLoader.classList.add('show');    
+}
+function hideLoader(){
+    divLoader.classList.remove('show');
+}
+
+planetsDiv.forEach(elem => {
     elem.addEventListener('click', queryInfo)
 })
+let planets = new Map();
+
+
 
 // request data from the server
 function queryInfo(e){
     let planetName = e.target.textContent;
-    // -----------to do map
-    let planet = {};
-
-    fetch('https://api.api-ninjas.com/v1/planets?name=' + planetName, {headers: { 'X-Api-Key': 'GHzlHKT4j4DKherfN381hw==mp2gk7umE4rNE2wF'}})
-        .then(res => res.json())
-        .then(result => {
-            planet = result[0];
-            console.log(planet)
-        })
-        .catch(error => {
-            console.log('No data')
-        }
-    )
-    return planet;
+    if (!planets.has(planetName)) {
+        let planet = {};
+        showLoader();        
+        fetch('https://api.api-ninjas.com/v1/planets?name=' + planetName, {headers: { 'X-Api-Key': 'GHzlHKT4j4DKherfN381hw==mp2gk7umE4rNE2wF'}})
+            .then(res => res.json())
+            .then(result => {
+                hideLoader();
+                planet = result[0];               
+                planets.set(planetName, planet);
+                showInfo(planetName);
+            })
+            .catch(error => {
+                console.log('No data')
+            }
+        )
+    } else{
+        showInfo(planetName);
+    }    
+}
+function showInfo (name){
+    let planet = planets.get(name);
+    console.log(planet)
+    
 }
 
 
